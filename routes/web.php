e1c6aed0c1873136fc36bd152d7c73192df4c80e
sirtheprogrammer <sirtheprogrammer@gmail.com>
2026-07-15 21:51:08 +0300

    fix the web

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $bootedAt = Cache::rememberForever('server_booted_at', fn () => now());
+    $bootedAt = Carbon::parse(Cache::rememberForever('server_booted_at', fn () => now()->toIso8601String()));
 
     return view('welcome', ['uptime' => $bootedAt->diffForHumans(null, true)]);
 });
