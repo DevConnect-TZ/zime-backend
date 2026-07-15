a4e023ee2b7f5a98fd3fa17c9ef7c4f3159112f1
sirtheprogrammer <sirtheprogrammer@gmail.com>
2026-07-15 21:36:55 +0300

    add server uptime

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -136,6 +136,10 @@
                             </svg>
                         </a>
                     </p>
+
+                    <p class="mt-2 text-[#706f6c] dark:text-[#A1A09A]">
+                        Server uptime: {{ $uptime }}
+                    </p>
                 </div>
                 <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/364] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
                     {{-- Laravel Logo --}}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $bootedAt = Cache::rememberForever('server_booted_at', fn () => now());
+
+    return view('welcome', ['uptime' => $bootedAt->diffForHumans(null, true)]);
 });
